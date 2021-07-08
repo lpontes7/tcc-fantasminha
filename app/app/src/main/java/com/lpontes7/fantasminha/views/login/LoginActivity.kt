0d9da17dd76418a039de8e4dfd55d809e1d61f35
lpontes7 <lucas.pontes.07@hotmail.com>
2021-07-08 02:00:20 -0300

    Algumas modificacoes no layout tela de login

--- app/app/src/main/java/com/lpontes7/fantasminha/views/login/LoginActivity.kt
+++ app/app/src/main/java/com/lpontes7/fantasminha/views/login/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.lpontes7.fantasminha.views.login
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 
 import com.lpontes7.fantasminha.databinding.ActivityLoginBinding
@@ -21,19 +23,19 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun validarCampos(){
+    private fun validarCampos() {
         val email = binding.editEmail.text.toString()
         val senha = binding.editSenha.text.toString()
 
-        if (email.isEmpty() || senha.isEmpty()){
-            if (email.isEmpty()) binding.editEmail.error = "Campo obrigatório"
-            if (senha.isEmpty()) binding.editSenha.error = "Campo obrigatório"
-        }else{
-           // autenticarUsuario()
+        if (email.isEmpty() || senha.isEmpty()) {
+            if (email.isEmpty()) Toast.makeText(this, "Preencha o email", Toast.LENGTH_SHORT).show()
+            if (senha.isEmpty()) Toast.makeText(this, "Preencha a senha", Toast.LENGTH_SHORT).show()
+        } else {
+            // autenticarUsuario()
         }
     }
 
-    private fun verificarUsuarioLogado(){
+    private fun verificarUsuarioLogado() {
     }
 
 }
